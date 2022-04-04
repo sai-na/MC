a1125b477a6bc0a3b987351c081916b9bae4166f
sainath378 <92171156+sainath378@users.noreply.github.com>
2022-04-04 10:56:20 +0530

    Exp-18

---
+++ avr  c programs/Exp-18/AVRStudio 5.1/Exp-18.c
@@ -0,0 +1,38 @@
+/*
+ * Exp_18.c
+ *
+ * Created: 04-04-2022 10:39:15
+ *  Author: SSM POLY
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define DATA1 7
+#define DATA2 3
+#define DATA3 0xff
+#define DATA4 0x01
+#define DATA5 5
+#define DATA6 2
+
+
+
+int main(void)
+
+{
+ DDRB = 0xff;// make port B output
+	   PORTB = DATA1 + DATA2;//ANDing
+	   _delay_ms(5000);
+	   PORTB = DATA3-DATA4;//ORing
+	   _delay_ms(5000);
+	   PORTB = DATA5 * DATA6;//XORing
+	   _delay_ms(5000);
+    while(1)
+    {
+       
+	   
+	   
+    }
+	return 0;
+}
+
